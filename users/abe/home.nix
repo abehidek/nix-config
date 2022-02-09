640e734c4c67a08696f5df74843350a7720139ad
hidek.abe@outlook.com <hidek.abe@outlook.com>
2022-02-09 19:17:36 -0300

    Initial commit

---
+++ users/abe/home.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "abe";
+  home.homeDirectory = "/home/abe";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs;  [
+    neofetch
+  ];
+}
