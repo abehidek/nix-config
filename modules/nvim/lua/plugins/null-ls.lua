12bbe8e8faef2607380c4332c47135a26ff8e4eb
abehidek <hidek.abe@outlook.com>
2022-04-28 23:29:04 -0300

    Enable semi by default in prettier null-ls config

--- modules/nvim/lua/plugins/null-ls.lua
+++ modules/nvim/lua/plugins/null-ls.lua
@@ -12,7 +12,7 @@ null_ls.setup({
 	debug = false,
 	sources = {
 		formatting.prettier.with({
-			extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+			extra_args = { "--single-quote", "--jsx-single-quote" },
 		}),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		-- formatting.yapf,
