39b9b735c53efb318f715fc10cad17bd2f4aec2a
abehidek <hidek.abe@outlook.com>
2022-05-03 21:29:14 -0300

    Format flake.nix using nixfmt

--- flake.nix
+++ flake.nix
@@ -3,36 +3,41 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    home-manager = { url = github:nix-community/home-manager; inputs.nixpkgs.follows = "nixpkgs"; };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
-    let 
+    let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
-      unstable = import nixpkgs-unstable { inherit system; config.allowUnfree = true; };
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      unstable = import nixpkgs-unstable {
+        inherit system;
+        config.allowUnfree = true;
+      };
       lib = nixpkgs.lib;
     in {
       nixosConfigurations = {
-        flex5i = lib.nixosSystem { 
+        flex5i = lib.nixosSystem {
           inherit system;
           specialArgs = { inherit unstable; };
           modules = [
             ./hosts/flex5i/system.nix
             ./hosts/system.nix
 
-            home-manager.nixosModules.home-manager {
-              home-manager =
-                let
-                  user = "abe";
-                in {
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = let user = "abe";
+              in {
                 useGlobalPkgs = true;
                 useUserPackages = true;
                 extraSpecialArgs = { inherit unstable user; };
                 users.abe = {
-                  imports = [
-                    ./hosts/flex5i/abe.nix
-                    ./hosts/home.nix
-                  ];
+                  imports = [ ./hosts/flex5i/abe.nix ./hosts/home.nix ];
                 };
               };
             }
