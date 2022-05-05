430334e1bc2a8b6e346480aa323923f64dc4b2b7
abehidek <hidek.abe@outlook.com>
2022-05-04 21:01:38 -0300

    Add comments in nord.nix base16

--- modules/theme/colorscheme/nord.nix
+++ modules/theme/colorscheme/nord.nix
@@ -1,19 +1,19 @@
 # Nord base16 theme
 {
-  base00 = "#2E3440";
-  base01 = "#3B4252";
-  base02 = "#434C5E";
-  base03 = "#4C566A";
-  base04 = "#D8DEE9";
-  base05 = "#E5E9F0";
-  base06 = "#ECEFF4";
-  base07 = "#8FBCBB";
-  base08 = "#88C0D0";
-  base09 = "#81A1C1";
-  base0A = "#5E81AC";
-  base0B = "#BF616A";
-  base0C = "#D08770";
-  base0D = "#EBCB8B";
-  base0E = "#A3BE8C";
-  base0F = "#B48EAD";
+  base00 = "#2E3440"; # Default Background
+  base01 = "#3B4252"; # Lighter Background (Used for status bars, line number and folding marks)
+  base02 = "#434C5E"; # Selection Background
+  base03 = "#4C566A"; # Comments, Invisibles, Line Highlighting
+  base04 = "#D8DEE9"; # Dark Foreground (Used for status bars)
+  base05 = "#E5E9F0"; # Default Foreground, Caret, Delimiters, Operators
+  base06 = "#ECEFF4"; # Light Foreground (Not often used)
+  base07 = "#8FBCBB"; # Light Background (Not often used)
+  base08 = "#88C0D0"; # Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
+  base09 = "#81A1C1"; # Integers, Boolean, Constants, XML Attributes, Markup Link Url
+  base0A = "#5E81AC"; # Classes, Markup Bold, Search Text Background
+  base0B = "#BF616A"; # Strings, Inherited Class, Markup Code, Diff Inserted
+  base0C = "#D08770"; # Support, Regular Expressions, Escape Characters, Markup Quotes
+  base0D = "#EBCB8B"; # Functions, Methods, Attribute IDs, Headings
+  base0E = "#A3BE8C"; # Keywords, Storage, Selector, Markup Italic, Diff Changed
+  base0F = "#B48EAD"; # Deprecated, Opening/Closing Embedded Language Tags, e.g. <?php ?>
 }
