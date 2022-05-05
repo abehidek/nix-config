72caf735d1b4c708ae3dcdb29be43d6371770096
abehidek <hidek.abe@outlook.com>
2022-05-04 23:01:38 -0300

    Copied buildScript.nix to lib (new folder)

---
+++ lib/buildScript.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+  name: src: env:
+  pkgs.stdenv.mkDerivation {
+    name = name;
+    script = pkgs.substituteAll (env // {
+      src = src;
+      isExecutable = true;
+    });
+    buildCommand = ''
+      install -Dm755 $script $out/bin/${name}
+      patchShebangs $out/bin/${name}
+    '';
+  }
