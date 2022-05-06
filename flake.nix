f8d39291af8c61e2f0703618e7e9503af86563bc
abehidek <hidek.abe@outlook.com>
2022-05-05 21:00:25 -0300

    Refactor flake.nix code

--- flake.nix
+++ flake.nix
@@ -8,41 +8,38 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-      unstable = import nixpkgs-unstable {
-        inherit system;
-        config.allowUnfree = true;
-      };
-      lib = nixpkgs.lib;
-    in {
-      nixosConfigurations = {
-        flex5i = lib.nixosSystem {
-          inherit system;
-          specialArgs = { inherit unstable; };
-          modules = [
-            ./hosts/flex5i/system.nix
-            ./hosts/system.nix
-
-            home-manager.nixosModules.home-manager
-            {
-              home-manager = let user = "abe";
-              in {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                extraSpecialArgs = { inherit unstable user; };
-                users.abe = {
-                  imports = [ ./hosts/flex5i/abe.nix ./hosts/home.nix ];
-                };
-              };
-            }
-          ];
-        };
-      };
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
     };
+    unstable = import nixpkgs-unstable {
+      inherit system;
+      config.allowUnfree = true;
+    };
+    lib = nixpkgs.lib;
+  in {
+    nixosConfigurations.flex5i = lib.nixosSystem {
+      inherit system;
+      specialArgs = { inherit unstable; };
+      modules = [
+        ./hosts/flex5i/system.nix
+        ./hosts/system.nix
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = let user = "abe";
+          in {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            extraSpecialArgs = { inherit unstable user; };
+            users.abe = {
+              imports = [ ./hosts/flex5i/abe.nix ./hosts/home.nix ];
+            };
+          };
+        }
+      ];
+    };
+  };
 }
