80dc49bf1d497065b57340df166f3c65f078c21b
abehidek <hidek.abe@outlook.com>
2022-05-05 23:30:49 -0300

    Add qt module to rf-modules

---
+++ rf-modules/themes/qt/default.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+{
+  # environment = {
+  #   systemPackages = with pkgs; [libsForQt5.qtstyleplugins ];
+  #   variables = {
+  #     "QT_STYLE_OVERRIDE" = "kvantum";
+  #   };
+  # };
+  # home-manager.users.abe.qt = {
+  #   enable = true;
+  #   platformTheme = "gtk";
+  # };
+  # home-manager.users.abe.home.packages = with pkgs; [
+  #   libsForQt5.qtstyleplugins
+  # ];
+}
