1826b90e36474792b0af8f6268d43ce70fa62565
abehidek <hidek.abe@outlook.com>
2022-05-08 21:35:25 -0300

    Enable pcmanfm bind

--- modules/desktop/sway/home.nix
+++ modules/desktop/sway/home.nix
@@ -116,6 +116,7 @@
       bindsym Print+c exec ${pkgs.sway-contrib.grimshot}/bin/grimshot --notify copy area
       bindsym Mod4+Print+s exec ${pkgs.sway-contrib.grimshot}/bin/grimshot --notify save active
       bindsym Mod4+Print+c exec ${pkgs.sway-contrib.grimshot}/bin/grimshot --notify copy active
+      bindsym Mod4+p exec ${pkgs.pcmanfm}/bin/pcmanfm
       for_window [title="Ulauncher"] border none
     '';
     #client.focused #2E3440 #2E3440 #ECEFF4 #2E3440 #2E3440
