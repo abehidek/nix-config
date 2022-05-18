7d9fbfbc7bc398b647aeb4425dc07aa880f83699
abehidek <hidek.abe@outlook.com>
2022-05-17 21:27:15 -0300

    Add borders to completion window nvim

--- modules/editors/neovim/lua/plugins/cmp.lua
+++ modules/editors/neovim/lua/plugins/cmp.lua
@@ -104,10 +104,9 @@ cmp.setup({
 		format = function(entry, vim_item)
 			-- Kind icons
 			vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-			-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
 			vim_item.menu = ({
-				nvim_lsp = "[LSP]",
 				luasnip = "[Snippet]",
+				nvim_lsp = "[LSP]",
 				buffer = "[Buffer]",
 				path = "[Path]",
 			})[entry.source.name]
@@ -125,6 +124,7 @@ cmp.setup({
 		select = false,
 	},
   window = {
+    completion = cmp.config.window.bordered(),
     documentation = {
       border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
     },
