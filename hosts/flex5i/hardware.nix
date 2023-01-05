3847d91d8b4845e6de0d922ac6f2a37eba6d1676
abehidek <hidek.abe@outlook.com>
2023-01-05 19:06:07 -0300

    feat(flex5i): add ntfs to supported fs

--- hosts/flex5i/hardware.nix
+++ hosts/flex5i/hardware.nix
@@ -11,6 +11,7 @@
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
+  boot.supportedFilesystems = [ "ntfs" ];
 
   fileSystems."/" = {
     device = "/dev/disk/by-label/NIXOS-ROOT";
