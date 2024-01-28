10ae64f1a2019bc58b93b668c26728165293a82d
abehidek <hidek.abe@outlook.com>
2024-01-28 00:32:29 -0300

    fix: lack of cedilla in us alt-intl kb
    - add qpwgraph pkg

--- systems/flex5i/default.nix
+++ systems/flex5i/default.nix
@@ -110,7 +110,24 @@ in {
 
   # Internalisation
   time.timeZone = "America/Sao_Paulo";
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LANGUAGE ="";
+      LC_ADDRESS = "pt_BR.UTF-8";
+      LC_COLLATE = "en_US.UTF-8";
+      LC_CTYPE = "pt_BR.UTF-8";
+      LC_IDENTIFICATION = "pt_BR.UTF-8";
+      LC_MEASUREMENT = "pt_BR.UTF-8";
+      LC_MESSAGES = "en_US.UTF-8";
+      LC_MONETARY = "pt_BR.UTF-8";
+      LC_NAME = "pt_BR.UTF-8";
+      LC_NUMERIC = "pt_BR.UTF-8";
+      LC_PAPER = "pt_BR.UTF-8";
+      LC_TELEPHONE = "pt_BR.UTF-8";
+      LC_TIME = "pt_BR.UTF-8";
+    };
+  };
   console = {
     font = "ter-i24b";
     packages = [ pkgs.terminus_font ];
@@ -221,7 +238,7 @@ in {
     brightnessctl
     pulseaudio
     pulseaudio-ctl
-    qjackctl
+    qjackctl qpwgraph
     # playerctl
     rofi
     gnome.dconf-editor
