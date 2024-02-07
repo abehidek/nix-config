ebbca19d7f0f3b38a4bec7d3484f9b67f531a167
abehidek <hidek.abe@outlook.com>
2024-02-07 22:25:08 +0000

    base-lxc: improvements

--- systems/base-lxc/default.nix
+++ systems/base-lxc/default.nix
@@ -1,14 +1,59 @@
-{ pkgs, inputs, outputs, lib, config, ...}: 
+{ pkgs, inputs, outputs, lib, config, modulesPath, ...}:
 
 {
-  services.sshd.enable = true;
-  services.nginx.enable = true;
+  imports = [
+    (modulesPath + "/virtualisation/proxmox-lxc.nix") # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/programs/git.nix
+    ../global
+  ];
 
-  networking.firewall.allowedTCPPorts = [80];
+  proxmoxLXC = {
+    privileged = false;
+    manageNetwork = false;
+    manageHostName = false;
+  };
+
+  systemd.suppressedSystemUnits = [
+    "dev-mqueue.mount"
+    "sys-kernel-debug.mount"
+    "sys-fs-fuse-connections.mount"
+  ];
+
+  boot.loader.grub.enable = false;
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 80 443 8080 ];
+  };
+
+  services.getty.autologinUser = lib.mkDefault "root";
+
+  programs = {
+    git.enable = true;
+  };
+
+
+  environment = {
+    variables = {
+      EDITOR = "hx";
+      VISUAL = "hx";
+    };
+
+    systemPackages = with pkgs; [
+      # tui
+      helix lazygit
+      # cli
+      lsof neofetch
+    ];
+  };
+
+  users.users = {
+    root.initialPassword = "nixos";
+    abe = {
+      isNormalUser = true;
+      initialPassword = "password";
+      extraGroups = [ "wheel" "video" "audio" ];
+    };
+  };
 
   system.stateVersion = lib.version;
-
-  users.users.root.password = "nixos";
-  services.openssh.settings.PermitRootLogin = lib.mkDefault "yes";
-  services.getty.autologinUser = lib.mkDefault "root";
 }
