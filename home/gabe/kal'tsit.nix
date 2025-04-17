ec36e9a96948e8a10594bf63dc5df66f98efa9df
abehidek <me@hidek.xyz>
2025-04-17 18:11:58 -0300

    feat(kal'tsit): enable direnv & nix-direnv w/ shell integrations
    - enable silent mode for direnv

--- home/gabe/kal'tsit.nix
+++ home/gabe/kal'tsit.nix
@@ -161,6 +161,15 @@ in
     };
   };
 
+  programs.direnv = {
+    enable = true;
+    silent = true;
+    nix-direnv.enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    enableNushellIntegration = true;
+  };
+
   programs.zoxide = {
     enable = true;
     enableBashIntegration = true;
