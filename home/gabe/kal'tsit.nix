83a53aea7bacac941c0eaf49676be4540cb18ec7
abehidek <me@hidek.xyz>
2026-03-12 13:36:14 -0300

    chore: disable hm ssh default cfg because future deprec

--- home/gabe/kal'tsit.nix
+++ home/gabe/kal'tsit.nix
@@ -75,7 +75,20 @@ in
 
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
     matchBlocks = {
+      "*" = {
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
+      };
       "git personal" = {
         host = "github.com";
         hostname = "github.com";
